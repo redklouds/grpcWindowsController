cff2074eb432335f195bf19b03186a4f348d73c8
Danny Ly <danny.ly.cs@outlook.com>
2019-10-10 16:56:17 -0700

    reset Master

--- FocusManager/Program.cs
+++ FocusManager/Program.cs
@@ -10,54 +10,11 @@ namespace FocusManager
     class Program
     {
 
-        static bool RUNNING = true;
+        
         [DllImport("USER32.DLL")]
         public static extern bool SetForegroundWindow(IntPtr hWnd);
         
-        public bool continueRunning;
-        List<Process> processList = new List<Process>();
-        public Program()
-        {
-            continueRunning = true;
-            setMonkeyProcessList();
 
-        }
-        private bool setMonkeyProcessList()
-        {
-            foreach (Process proc in Process.GetProcesses())
-            {
-                Console.WriteLine(proc.ProcessName);
-                if (proc.ProcessName.Contains("elementclient"))
-                {
-
-                    processList.Add(proc);
-                    //proc.WaitForInputIdle();
-
-                }
-            }
-            return true;
-        }
-
-        public bool setFunningFlag(bool continueToRun)
-        {
-            this.continueRunning = continueToRun;
-            return true;
-        }
-
-        public async Task Run()
-        {
-            while (this.continueRunning)
-            {
-                foreach (Process proc in this.processList)
-                {
-                    IntPtr windowPtr = proc.MainWindowHandle;
-                    SetForegroundWindow(windowPtr);
-                    var isResponding = proc.Responding;
-                    //Console.WriteLine($"is resonding? {isResponding}");
-                    Thread.Sleep(1000);
-                }
-            }
-        }
 
 
         static void Main(string[] args)
@@ -78,7 +35,7 @@ namespace FocusManager
                 }
             }
             Console.WriteLine($"There exist {procList.Count} Clients");
-            ConsoleKey UserInput = ConsoleKey.Y;
+
             while (true)
             {
                 foreach(Process proc in procList)
@@ -87,7 +44,7 @@ namespace FocusManager
                     SetForegroundWindow(windowPtr);
                     var isResponding = proc.Responding;
                     //Console.WriteLine($"is resonding? {isResponding}");
-                    Thread.Sleep(1000);
+                    Thread.Sleep(500);
                 }
             }
                
