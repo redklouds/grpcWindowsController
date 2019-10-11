7fe3bb8ac2fba42ffa173233611aad7935d1f5ff
Danny Ly <danny.ly.cs@outlook.com>
2019-10-10 20:52:00 -0700

    oraganized code

--- FocusManager/Program.cs
+++ FocusManager/Program.cs
@@ -15,39 +15,92 @@ namespace FocusManager
         public static extern bool SetForegroundWindow(IntPtr hWnd);
         
 
-
+       public static int IncrementCurrentNum(int currentNum)
+        {
+            //increment by 3
+            return currentNum + 2;
+        }
 
         static void Main(string[] args)
         {
-          
-            List<Process> procList = new List<Process>();
-            foreach (Process proc in Process.GetProcesses())
-            {
-                //Console.WriteLine(proc.ProcessName);
-                if (proc.ProcessName.Contains("elementclient") || proc.ProcessName.Contains("Monkey"))
-                {
+            /*
+               //get the processes
+               List<Process> procList = new List<Process>();
+               foreach (Process proc in Process.GetProcesses())
+               {
+                   //Console.WriteLine(proc.ProcessName);
+                   if (proc.ProcessName.Contains("elementclient") || proc.ProcessName.Contains("Monkey"))
+                   {
+                       //change the priority
 
-                    procList.Add(proc);
-                    //proc.WaitForInputIdle();
-                    IntPtr monkeyPtr = proc.MainWindowHandle;
-                    SetForegroundWindow(monkeyPtr);
-                  
-                }
-            }
-            Console.WriteLine($"There exist {procList.Count} Clients");
+                       proc.PriorityClass = ProcessPriorityClass.High;
+                       procList.Add(proc);
+                       //proc.WaitForInputIdle();
+                       IntPtr monkeyPtr = proc.MainWindowHandle;
+                       SetForegroundWindow(monkeyPtr);
 
+                   }
+               }
+               Console.WriteLine($"There exist {procList.Count} Clients");
+
+               while (true)
+               {
+                   foreach(Process proc in procList)
+                   {
+                       Console.WriteLine($"Switching Processes to: {proc.Id}");
+                       IntPtr windowPtr = proc.MainWindowHandle;
+                       SetForegroundWindow(windowPtr);
+                       var isResponding = proc.Responding;
+                       //Console.WriteLine($"is resonding? {isResponding}");
+                       Thread.Sleep(3000);
+                   }
+               }
+           */
+
+            FocusManager fm = new FocusManager();
+            fm.setRunningStatus(true);
+            //Task.Run(()=> fm.RUN());
+
+   
             while (true)
             {
-                foreach(Process proc in procList)
+                Console.WriteLine("Press 'Y' to continue running or 'N' to stop");
+                ConsoleKey input = Console.ReadKey().Key;
+                if(input == ConsoleKey.Y)
                 {
-                    IntPtr windowPtr = proc.MainWindowHandle;
-                    SetForegroundWindow(windowPtr);
-                    var isResponding = proc.Responding;
-                    //Console.WriteLine($"is resonding? {isResponding}");
-                    Thread.Sleep(500);
+                    fm.setRunningStatus(true);
+                }
+                if(input == ConsoleKey.N)
+                {
+                    fm.setRunningStatus(false);
+                }
+                if(input == ConsoleKey.I)
+                {
+                    //incrementtime
+                    var cur = fm.getSwitchingTimeIntervalInSeconds();
+                    Console.WriteLine($"Current Value: {cur}");
+
+                    var setThisAmount = IncrementCurrentNum(cur);
+                    
+                    fm.setSwitchingIntervals(setThisAmount);
+                }
+
+                if(input == ConsoleKey.D)
+                {
+                    var cur = fm.getSwitchingTimeIntervalInSeconds();
+                    Console.WriteLine($"Current Value: {cur}");
+                    var setThisAmount = cur - 1;
+                    if(setThisAmount > 0)
+                    {
+                        fm.setSwitchingIntervals(setThisAmount);
+                    }
+             
                 }
             }
-               
+            Console.WriteLine("DFDFSD");
+        
+        
         }
+
     }
 }
